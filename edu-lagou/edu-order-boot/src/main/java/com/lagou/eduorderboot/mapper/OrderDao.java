abc4683f77d06204f20bc1f08d2c151fa6951679
fycz119 <1198117429@qq.com>
2022-04-23 10:20:32 +0800

    no message

--- edu-lagou/edu-order-boot/src/main/java/com/lagou/eduorderboot/mapper/OrderDao.java
+++
@@ -1,9 +0,0 @@
-package com.lagou.eduorderboot.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lagou.eduorderboot.entity.UserCourseOrder;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface OrderDao extends BaseMapper<UserCourseOrder> {
-}
